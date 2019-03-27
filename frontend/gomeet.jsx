083c7c156c999deb3411970a0ce553dad2ce6e0e
Polina Vulakh <pvulakh@gmail.com>
2019-03-26 21:58:10 -0400

    Finish redux

--- frontend/gomeet.jsx
+++ frontend/gomeet.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, signup, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
    
     //TESTING
     window.login = login;
     window.signup = signup;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     //TESTING
 
     ReactDOM.render(<h1>Meet GoMeet!</h1>, root);
