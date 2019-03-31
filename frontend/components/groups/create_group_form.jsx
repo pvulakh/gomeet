3db531edc0c6ba401c800ae0b4f80791aa005568
pvulakh <pvulakh@gmail.com>
2019-03-31 01:40:21 -0400

    Start styling create group form

--- frontend/components/groups/create_group_form.jsx
+++ frontend/components/groups/create_group_form.jsx
@@ -32,22 +32,25 @@ class CreateGroupForm extends React.Component {
         />
 
         <div className='create-msg'>
-          <h2>Start a new GoMeet</h2>
-          <h3>We'll help you find the right people to make it happen.</h3>
+          <img src="https://secure.meetupstatic.com/img/start_v2/dweb_hero.jpg" />
+          <div className='create-text'>
+            <h2>Start a new GoMeet</h2>
+            <h3>We'll help you find the right people to make it happen.</h3>
+          </div>
         </div>
 
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className='create-form'>
           <div className='create-location'>
             <img src="https://secure.meetupstatic.com/s/img/5771697722992842330638/start_v2/globe.svg"></img>
-            <div className='create-text'></div>
+            <div className='create-loc-text'>
               <div className='create-step'>
                 step 1 of 4
               </div>
-              <h5>What's your new GoMeet Group's hometown?</h5>
+              <h5 className='create-group-heading'>What's your new GoMeet Group's hometown?</h5>
               <div className='city'>
-                <input type='text' defaultValue='New York, NY'/>
-                <div>(change location)</div>
+                <input type='text' defaultValue='New York, NY'/>(<div>change location</div>)
               </div>
+            </div>
           </div>
 
           <div className='create-main'>
@@ -56,14 +59,14 @@ class CreateGroupForm extends React.Component {
               <div className='create-step'>
                 step 2 of 4
               </div>
-              <h5>What will your GoMeet's name be?</h5>
+              <h5 className='create-group-heading'>What will your GoMeet's name be?</h5>
               <div className='create-title'>
                 <input type='text' value={this.state.title} placeholder="example: New York Hiking GoMeet" onChange={this.handleChange('title')}/>
               </div>
               <div className='create-step'>
                   step 3 of 4
               </div>
-              <h5>Describe who should join, and what your GoMeet will do.</h5>
+              <h5 className='create-group-heading'>Describe who should join, and what your GoMeet will do.</h5>
               <div className='create-description'>
                   <input type='text' value={this.state.description} onChange={this.handleChange('description')}/>
               </div>
@@ -74,7 +77,7 @@ class CreateGroupForm extends React.Component {
               <div className='create-step'>
                 step 4 of 4
               </div>
-              <h5>What it means to be a GoMeet</h5>
+              <h5 className='create-group-heading'>What it means to be a GoMeet</h5>
               <ul>
                 <li>Real, in-person conversations</li>
                 <li>Open and honest intentions</li>
