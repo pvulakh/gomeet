e1c80d619ab7f6860040c5b1cb607f58c6e4cab2
pvulakh <pvulakh@gmail.com>
2019-04-03 09:05:43 -0400

    Attempt memberships in react redux

--- frontend/reducers/groups_reducer.js
+++ frontend/reducers/groups_reducer.js
@@ -9,6 +9,8 @@ export default (state = {}, action) => {
     case RECEIVE_GROUP:
       return merge({}, state, {[action.group.id]: action.group});
       //memberships?????? how/where/is it automatic? test tomorrow
+
+      //where do memberships even go in the state??????? is it just an array of ids in groups? then it is automatic since action.group has a memberships key......
     case REMOVE_GROUP:
       const newState = merge({}, state);
       delete newState[action.groupId];
