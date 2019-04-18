7ee9c9411da10b2f0c8c36a13cbef9888a06b40f
Polina Vulakh <pvulakh@gmail.com>
2019-04-18 11:07:57 -0400

    Start events

--- frontend/components/events/create_event_form.jsx
+++ frontend/components/events/create_event_form.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { withRouter, Route, Redirect } from 'react-router-dom';
+//use moment.js and react datepicker
 
 class CreateEventForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.event;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+  componentDidMount() {
+    if (!this.props.groupTitle) {
+      this.props.fetchGroup(groupId);
+    }
+  }
 
+  handleChange(field) {
+    return e => this.setState({[field]: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createEvent(this.props.groupId, this.state);
+    // history.push to events show page once built out
+  }
+
+  render() {
+    if (!this.props.groupTitle) {
+      return;
+    }
+    
+    return (
+      <div className='schedule-page'>
+          <Route
+            path="/schedule"
+            render={
+              () => {
+                if (!this.props.currentUser) {
+                  return <Redirect to="/login" />
+                }
+              }
+            }
+          />
+        </div>
+    );
+  }
 }
 
-export default CreateEventForm;
+export default withRouter(CreateEventForm);
