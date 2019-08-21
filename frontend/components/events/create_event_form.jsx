a8b12e552bb851e5a372900be6463f676d8241de
Polina Vulakh <pvulakh@gmail.com>
2019-08-20 23:15:44 -0400

    Incorporate DatePicker for creating events

--- frontend/components/events/create_event_form.jsx
+++ frontend/components/events/create_event_form.jsx
@@ -23,7 +23,13 @@ class CreateEventForm extends React.Component {
     return e => {
       debugger
       return this.setState({[field]: e.target.value});
-    }
+    };
+  }
+
+  handleDate(field) {
+    return date => {
+      return this.setState({[field]: date });
+    };
   }
 
   handleSubmit(e) {
@@ -32,7 +38,7 @@ class CreateEventForm extends React.Component {
 
     // use this.state.startDate/this.state.startTime to create the actual dateTime objects that we'll want to push into db
     // for formData.append('event[startTime]') and fromData.append('event['endTime'])
-
+    debugger
     this.props.createEvent(this.props.groupId, this.state).then(() => this.props.history.push(`/${this.props.groupId}`));;
     // history.push to events show page once built out; for now, goes to group show
   }
@@ -41,7 +47,21 @@ class CreateEventForm extends React.Component {
     if (!this.props.groupTitle) {
       return null;
     }
-    
+    debugger
+
+    let endDate, endSection;
+    if (endDate) {
+      endSection = (
+        // <div>
+          // <DatePicker
+          //   className='date-pick'
+          //   selected={this.state.endDate}
+          //   onChange={this.handleDate('endDate')}
+          // />
+          {/* <input type='time' value={this.state.endTime} onChange={this.handleChange('endTime')} /> */}
+        // </div>
+      );
+    }
     return (
       <div className='schedule-page'>
         <Route
@@ -69,13 +89,21 @@ class CreateEventForm extends React.Component {
           <label className='create-event-dt'>Date and time</label>
           <DatePicker
             className='date-pick'
-            selected={this.state.date}
-            onChange={this.handleChange('date')}
+            selected={this.state.startDate}
+            onChange={this.handleDate('startDate')}
           />
+          <input type='time' value={this.state.startTime} onChange={this.handleChange('startTime')} />
         </div>
         </form>
-        
-          <h2>{moment().format("MMM Do YY")}</h2>
+        <select onChange={this.handleChange('endTime')}> 
+          {/* e.target[e.target.selectedIndex].value to get the value of the option */}
+          <option value='1 hour'>1 hour</option>
+          <option value='1.5 hours'>1.5 hours</option>
+          <option value='2 hours' selected>2 hours</option>
+          <option value='3 hours'>3 hours</option>
+          <option onClick={() => endDate = true} value='end time'>Set an end time...</option>
+        </select>
+          {/* <h2>{moment().format("MMM Do YY")}</h2> */}
       </div>
     );
   }
